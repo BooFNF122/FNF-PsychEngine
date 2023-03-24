b055c63f04a060ddeca9bace5f4ada130d80179e
BooFNF122 <102209201+BooFNF122@users.noreply.github.com>
2023-03-24 07:29:03 +0700

    Add files via upload

---
+++ assets/preload/data/kutchapurgation/script.lua
@@ -0,0 +1,21 @@
+function goodNoteHit()
+   setProperty('health', getProperty('health')+0.015);
+   end
+
+function opponentNoteHit()
+       health = getProperty('health')
+    if getProperty('health') > 0.1 then
+       setProperty('health', health- 0.02);
+	end
+end
+
+function onUpdate(elapsed)
+	if dadName == 'EXKutcha' then --replace the name for your character name
+	  if curStep >= 0 then
+	songPos = getSongPosition()
+  local currentBeat = (songPos/500)*(bpm/80)
+  doTweenY(dadTweenY, 'dad', 50-30*math.sin((currentBeat*0.25)*math.pi),0.001)
+  end
+end
+end
+  
---
+++ assets/preload/data/kutchapurgation/script2.lua
@@ -0,0 +1,3 @@
+function opponentNoteHit()
+triggerEvent('Screen Shake','0.1, 0.025','0, 0')
+	end
